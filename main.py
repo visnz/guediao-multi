3c004a06eabcee76ff707c49039688826261fa52
visnz <visn0518@gmail.com>
2018-09-27 17:20:50 +0800

    gitignore tempfile; main bot.register test

--- main.py
+++ main.py
@@ -3,20 +3,36 @@ import threading
 from time import sleep
 from datetime import datetime
 
-def guediaobot():
-    bot = Bot(None,2,None,None,None,None)
-    #mainClient=bot.groups().search('一盘炒粿条的故事')[0]
-    myself = bot.self
-    t1=threading.Thread(target = heartbeat ,args=(myself,),name= 'heartbeat')
-    t1.start()
-    t1.join()
+
+bot = Bot('./tempfile',2,None,None,None,None)
+
 
 def heartbeat(reciver):
     while True:
-        sleep(5)
-        reciver.send('Heartbeat msg : ',datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        sleep(15)
+        print('heartbeat')
+        reciver.send('Heartbeat msg : '+ datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
-if __name__ == '__main__':
-    guediaobot()
+tuling = Tuling (api_key='739f8aed47d34dd78fa46de2085527df')
+#mainClient=bot.groups().search('一盘炒粿条的故事')[0]
+myself = bot.self
+#t1=threading.Thread(target = heartbeat ,args=(myself,),name= 'heartbeat')
+#t1.start()
+#t1.join()
+tester = bot.friends().search('高晓思')[0]
+
+
+@bot.register()
+def printall(msg):
+    print(msg)
+    return 
+    
+@bot.register([tester],TEXT)
+def autochat(msg):
+    print('msg we recieve: '+msg)
+    return tuling.do_reply(msg)
+
+embed()
+
 # mainClient.send('这里是来自bot的测试信息')
